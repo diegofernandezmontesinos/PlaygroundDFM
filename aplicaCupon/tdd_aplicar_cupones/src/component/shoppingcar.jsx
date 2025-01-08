bf80421ea2725a4745556bb1c44de04dc738e771
Diego <dfmontesinos@gmail.com>
2025-01-08 20:33:41 +0100

    finish exercise

--- aplicaCupon/tdd_aplicar_cupones/src/component/shoppingcar.jsx
+++ aplicaCupon/tdd_aplicar_cupones/src/component/shoppingcar.jsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ShoppingCar = ({ onAddNumber, onAddCoupon }) => {
+  const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [inputCupon, setInputCupon] = useState("");
+
+  const handleAddNumber = () => {
+    const value = parseFloat(inputValue);
+    if (!isNaN(value)) {
+      setItems((prev) => [...prev, value]);
+      setTotal((prev) => prev + value);
+    }
+
+    setInputValue("");
+  };
+
+  const handleCupon = () => {
+    if (inputCupon.endsWith("EUROSOFF")) {
+      const fixedDiscount = parseFloat(inputCupon.replace("EUROSOFF", ""));
+      if (!isNaN(fixedDiscount)) {
+        setTotal(Math.max(0, total - fixedDiscount));
+      }
+    } else if (inputCupon.endsWith("%OFF")) {
+      const percentageDiscount = parseFloat(inputCupon.replace("%OFF", ""));
+      if (!isNaN(percentageDiscount)) {
+        const discountAmount = (total * percentageDiscount) / 100;
+        setTotal(Math.max(0, total - discountAmount));
+      }
+    }
+    setInputCupon("");
+  };
+
   return (
     <>
       <h2>Shopping Car</h2>
-      <input type="text" placeholder="Enter a number" />
-      <button onClick={onAddNumber}>Add number</button>
-      <input type="text" placeholder="Enter a cupon" />
-      <button onClick={onAddCoupon}>Add a cupon</button>
+      <p>Total: €{total.toFixed(2)}</p>
+      <input
+        type="text"
+        placeholder="Enter a number"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddNumber}>Add number</button>
+      <input
+        type="text"
+        placeholder="Enter a cupon"
+        value={inputCupon}
+        onChange={(e) => setInputCupon(e.target.value)}
+      />
+      <button onClick={handleCupon}>Add a cupon</button>
+
+      <ul>
+        {items.map((item, index) => {
+          <li key={index}>€ {item.toFixed(2)} </li>;
+        })}
+      </ul>
     </>
   );
 };
